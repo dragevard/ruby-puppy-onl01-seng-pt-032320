314d026bc5dc8b391d3707ab2812e1ca679dc466
Derek phung <dragevards@gmail.com>
2020-04-24 17:51:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,27 @@
 # Add your code here
+
+class Dog 
+  
+  @@all = []
+  
+  def initialize(puppy)
+    self.save
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def self.print_all
+    @@all.each do |x|
+      puts x 
+    end 
+  end 
+  
+  def save=(puppy)
+    @@all << self 
+  end 
+  
+  def self.clear_all
+    @@
+end
